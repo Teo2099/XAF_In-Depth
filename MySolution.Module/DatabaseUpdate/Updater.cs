b2fef0847a0999e1cc6cfd0f1ab0c68a2879d07f
Teo <Teo@DESKTOP-7JUSON1>
2024-11-10 22:19:47 +0200

    Add project files.

---
+++ MySolution.Module/DatabaseUpdate/Updater.cs
@@ -0,0 +1,34 @@
+﻿using DevExpress.ExpressApp;
+using DevExpress.Data.Filtering;
+using DevExpress.Persistent.Base;
+using DevExpress.ExpressApp.Updating;
+using DevExpress.ExpressApp.EF;
+using DevExpress.Persistent.BaseImpl.EF;
+using Microsoft.Extensions.DependencyInjection;
+using MySolution.Module.BusinessObjects;
+
+namespace MySolution.Module.DatabaseUpdate;
+
+// For more typical usage scenarios, be sure to check out https://docs.devexpress.com/eXpressAppFramework/DevExpress.ExpressApp.Updating.ModuleUpdater
+public class Updater : ModuleUpdater {
+    public Updater(IObjectSpace objectSpace, Version currentDBVersion) :
+        base(objectSpace, currentDBVersion) {
+    }
+    public override void UpdateDatabaseAfterUpdateSchema() {
+        base.UpdateDatabaseAfterUpdateSchema();
+		Employee employeeMary = ObjectSpace.FirstOrDefault<Employee>(x => x.FirstName == "Mary" && x.LastName == "Tellitson");
+		if (employeeMary == null)
+		{
+			employeeMary = ObjectSpace.CreateObject<Employee>();
+			employeeMary.FirstName = "Mary";
+			employeeMary.LastName = "Tellitson";
+			employeeMary.Email = "tellitson@example.com";
+			employeeMary.Birthday = new DateTime(1980, 11, 27);
+		}
+
+		ObjectSpace.CommitChanges(); //Uncomment this line to persist created object(s).
+	}
+    public override void UpdateDatabaseBeforeUpdateSchema() {
+        base.UpdateDatabaseBeforeUpdateSchema();
+    }
+}
---
+++ MySolution.Module/Migrations/20241110172009_MyInitialMigrationName4.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MySolution.Module.Migrations
+{
+    /// <inheritdoc />
+    public partial class MyInitialMigrationName4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "PhoneNumberID",
+                table: "Employees",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "PhoneNumbers",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Number = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneType = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PhoneNumbers", x => x.ID);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_PhoneNumberID",
+                table: "Employees",
+                column: "PhoneNumberID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_PhoneNumbers_PhoneNumberID",
+                table: "Employees",
+                column: "PhoneNumberID",
+                principalTable: "PhoneNumbers",
+                principalColumn: "ID",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_PhoneNumbers_PhoneNumberID",
+                table: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "PhoneNumbers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Employees_PhoneNumberID",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNumberID",
+                table: "Employees");
+        }
+    }
+}
